41395a425243c7a65cd565b9d88e789097e98d9f
tjdans345 <tjdans34568@gmail.com>
2021-06-13 23:51:52 +0900

    메인 화면, 업로드 화면 디자인 완료

--- lib/src/components/youtube_bottom_sheet.dart
+++ lib/src/components/youtube_bottom_sheet.dart
@@ -21,18 +21,26 @@ class YoutubeBottomSheet extends StatelessWidget {
   Widget _itemButton(
       {String icon, double iconSize, String label, Function() onTap}) {
     return InkWell(
-      onTap: () {},
+      onTap: onTap,
       child: Row(
         children: [
           Container(
-            width: 17,
-            height: 17,
-            child: SvgPicture.asset("assets/svg/icons/upload.svg"),
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle, color: Colors.grey.withOpacity(0.3)),
+            child: Center(
+              child: Container(
+                width: iconSize,
+                height: iconSize,
+                child: SvgPicture.asset("assets/svg/icons/$icon.svg"),
+              ),
+            ),
           ),
           SizedBox(
             width: 15.0,
           ),
-          Text("동영상 업로드"),
+          Text(label),
         ],
       ),
     );
@@ -54,13 +62,22 @@ class YoutubeBottomSheet extends StatelessWidget {
               height: 10.0,
             ),
             _itemButton(
-                icon: "upload.svg",
+                icon: "upload",
                 iconSize: 17,
                 label: "동영상 업로드",
                 onTap: () {
                   print("동영상 업로드 기능");
                 }),
-            _itemButton(),
+            SizedBox(
+              height: 10.0,
+            ),
+            _itemButton(
+                icon: "broadcast",
+                iconSize: 25,
+                label: "실시간 스트리밍 시작",
+                onTap: () {
+                  print("실시간 스트리밍 시작 기능");
+                }),
           ],
         ),
       ),
